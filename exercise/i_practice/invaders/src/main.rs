8fbc93c7e35d8f92b6c659d2057533ae1e54db87
kay <kay1216@yonsei.ac.kr>
2024-06-13 21:23:38 +0900

    i invaders

--- exercise/i_practice/invaders/src/main.rs
+++ exercise/i_practice/invaders/src/main.rs
@@ -1,17 +1,35 @@
-use std::error::Error;
+use std::{error::Error, io};
+use crossterm::{cursor::{Show, Hide}, terminal::{self, EnterAlternateScreen, LeaveAlternateScreen}, ExecutableCommand};
 use rusty_audio::Audio;
+use std::env;
 
 fn main() -> Result<(), Box<dyn Error>>{
 
     let mut audio = Audio::new();
+    let default_path = format!("{}/audio",env!("CARGO_MANIFEST_DIR"));
+    // println!("{}",default_path);
 
-    audio.add("explode","explode.wav");
-    audio.add("lose","lose.wav");
-    audio.add("move","move.wav");
-    audio.add("pew","pew.wav");
-    audio.add("win","win.wav");
+    for item in &["explode", "lose", 
+    "move", "pew", "startup","win"]{
+        audio.add(item, &format!("{}/{}.wav",default_path,item));
+        // println!("{}/{}.wav",default_path, item);
+    }
+
+
+    audio.play("startup");
     
+
+    // Terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
     audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
+
     Ok(())
 
 }
