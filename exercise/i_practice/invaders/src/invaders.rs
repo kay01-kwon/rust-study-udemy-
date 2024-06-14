31f2a33a95080cc0aa0a7c298780f8cbe57690c7
kay <kay1216@yonsei.ac.kr>
2024-06-15 00:00:36 +0900

    invader done

--- exercise/i_practice/invaders/src/invaders.rs
+++ exercise/i_practice/invaders/src/invaders.rs
@@ -32,6 +32,7 @@ impl Invaders
                 && (y < 9)
                 && (x%2 == 0)
                 && (y%2 == 0)
+                // && (x == 2)
                 {
                     army.push(Invader{x, y});
                 }
@@ -104,6 +105,35 @@ impl Invaders
             return true;
         }
         false
+    }
+
+    pub fn all_killed(&self) -> bool
+    {
+        self.army.is_empty()
+    }
+
+    pub fn reached_bottom(&self) -> bool
+    {
+        self.army
+        .iter()
+        .map(|invader| invader.y)
+        .max().unwrap_or(0) >= NUM_ROWS - 1
+    }
+
+    pub fn kill_invader_at(&mut self, x:usize, y:usize) -> bool
+    {
+        if let Some(idx) = self
+        .army
+        .iter()
+        .position(|invader| (invader.x == x) && (invader.y == y))
+        {
+            self.army.remove(idx);
+            true
+        }
+        else
+        {
+            false
+        }
     }
 }
 
--- exercise/i_practice/invaders/src/player.rs
+++ exercise/i_practice/invaders/src/player.rs
@@ -1,5 +1,6 @@
 use std::time::Duration;
 
+use crate::invaders::Invaders;
 use crate::{frame::Drawable, NUM_COLS, NUM_ROWS};
 use crate::shot::Shot;
 
@@ -56,6 +57,25 @@ impl Player{
             shot.update(delta);
         }
         self.shots.retain(|shot| !shot.dead());
+    }
+
+    pub fn detect_hits(&mut self, invaders:&mut Invaders) -> bool
+    {
+        let mut hit_something = false;
+        
+        for shot in self.shots.iter_mut()
+        {
+            if !shot.exploding 
+            {
+                if invaders.kill_invader_at(shot.x, shot.y)
+                {
+                    hit_something = true;
+                    shot.explode();
+                }
+                
+            }
+        }
+        hit_something
     }
 }
 
